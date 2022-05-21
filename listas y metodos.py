ec42dcf3d7b33f4b80442c22122f2db9c412fb85
arturo <arturogomezmurillo@gmail.com>
2022-05-21 17:17:28 -0500

    actualizacion

---
+++ listas y metodos.py
@@ -0,0 +1,24 @@
+"""nombres = []
+edades = []
+rango = range (0,5)
+for i in rango:
+    nombre = input("porfavor dijite su nombre : ")
+    edad = int(input("porfavor dijite su edad: "))
+    nombres.append(nombre)
+    edades.append(edad)
+print("las personas mayores de edad son")
+for i in rango:
+    if edades[i]>=18:
+        print(nombres[i])"""
+#lista compuestas
+lista=[[1,2,3][4,5,6][7,8,9][10,11,12]]
+print(lista)
+#imprimir listas internas
+print(lista[2])
+#imprimir componentes linstas internas
+print(lista[3][1])
+#imprimir todos los componentes de las listas internas
+rango = range(len(lista[1]))
+for i in range:
+    print(lista[1][i])
+
---
+++ primercodpy.py
@@ -0,0 +1,7 @@
+
+tommi = "Perro"
+año = 2022
+meta_casa = 10
+año_casa = año + meta_casa
+print(año_casa)
+print(tommi)
