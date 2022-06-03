6a944b844b4ebb72d44f952b0704e2941c10ad74
arturo <arturogomezmurillo@gmail.com>
2022-06-03 15:29:49 -0500

    organizar

--- ejerciciopesoenkilos.py
+++
@@ -1,6 +0,0 @@
-peso_en_libras = float(input("ingrese su peso en libras : "))
-def peso_libras_a_kilos (peso_en_libras):
-    libras_a_kilos = peso_en_libras / 2.2046
-    return f"su peso en kilos es {float(round(libras_a_kilos))}"    
-
-print (peso_libras_a_kilos(peso_en_libras))
--- tuplas.py
+++
@@ -1,3 +0,0 @@
-#tuplas 
-tupla=(1,2,3,4)
-tupla= 1,,2,3,4
