5374ef32feafe4279c2f6ff5411c3df5027610d5
arturo <arturogomezmurillo@gmail.com>
2022-08-24 09:52:35 -0500

    Ciclo3

--- Java/Libros/src/Modelo/DAO/GuardarLibro.java
+++ Java/Libros/src/Modelo/DAO/GuardarLibro.java
@@ -3,17 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelo.DAO;
-import basesdedatoslibros. *;
-import java.sql.*;
+
 /**
  *
  * @author artur
  */
 public class GuardarLibro {
-    public void guardar (int isbn, String titulo, int año){
-        
-    Conexion cc = new Conexion();
-    Connection cn = cc.conectar();}
-    PreparedStatement ps = cn.prepareStatement("INSERT INTO libro VALUES (?,?,?)");
     
 }
